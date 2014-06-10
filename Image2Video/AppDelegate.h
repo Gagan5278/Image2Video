6123582bb7cee288c34961e64808e02475ba7734
Gagan.Mishra <gagan@AQRGGNF12W5.local>
2014-06-10 14:31:39 +0530

    Initial Commit

---
+++ Image2Video/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Image2Video
+//
+//  Created by Gagan on 10/06/14.
+//  Copyright (c) 2014 Gagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
